cd8724ddc054fa07e736ca13146c0a6ebb4957fd
Nathan <nathan.clough@ttu.edu>
2022-02-16 19:21:24 -0600

    removed old test

--- kraken-app/src/App.test.js
+++ kraken-app/src/App.test.js
@@ -1,9 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import Home from './Pages/Home';
 
-test('renders learn react link', () => {
-  render(<Home />);
-  const linkElement = screen.getByText(/learn react/i);
-  //expect(false).toBeTruthy();
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   render(<Home />);
+//   const linkElement = screen.getByText(/learn /i);
+//   //expect(false).toBeTruthy();
+//   expect(linkElement).toBeInTheDocument();
+// });
