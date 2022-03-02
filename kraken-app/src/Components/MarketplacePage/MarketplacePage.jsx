1096eeeabcfafa3b3e588fcfcf7f97408c3c051d
krystyna-urbanczyk <krystyna.urbanczyk@ttu.edu>
2022-03-02 17:46:10 -0600

    moved card data into data.js but images won't render, tried to get pagination to work but I'm stuck...

--- kraken-app/src/Components/MarketplacePage/MarketplacePage.jsx
+++ kraken-app/src/Components/MarketplacePage/MarketplacePage.jsx
@@ -3,30 +3,9 @@ import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import NFTCard from '../NFTCard/NFTCard.jsx';
-import KrakNFT0 from '../../KrakNFT0.png';
-import KrakNFT1 from '../../KrakNFT1.png';
-import KrakNFT2 from '../../KrakNFT2.png';
-
-const cards = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+import { data } from '../../CardData.js';
 
 function MarketplacePage(props) {
-
-  /*const [page, setPage] = React.useState(1);
-  const handleChange = (event, value) => {
-    setPage(value);
-  };*/
-
-  const getImage = (index) => {
-    if (index === 0 ){
-      return KrakNFT0
-    }
-    else if (index === 1){
-      return KrakNFT1
-    }
-    else{
-      return KrakNFT2;
-    }
-  }
 
   return (
     <>
@@ -34,9 +13,9 @@ function MarketplacePage(props) {
           <Typography variant="h6" gutterBottom component="div">
             KrakNFT Marketplace
           </Typography>
-          <Box sx={{ display: 'flex', flexWrap: 'wrap' }}> 
-            {cards.map((card, index) => (
-              <NFTCard name={`Nft ${index}`} image={getImage(index)}/>
+          <Box sx={{ display: 'flex', flexWrap: 'wrap'}}> 
+            {data.map((cards, key) => (
+              <NFTCard key={data} name={data.name} image={data.image}/>
             ))}
           </Box>
         </Container>
--- kraken-app/src/Components/MarketplacePreview/MarketplacePreview.jsx
+++ kraken-app/src/Components/MarketplacePreview/MarketplacePreview.jsx
@@ -4,37 +4,22 @@ import Container from '@mui/material/Container';
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
 import NFTCard from '../NFTCard/NFTCard.jsx';
-import KrakNFT0 from '../../KrakNFT0.png';
-import KrakNFT1 from '../../KrakNFT1.png';
-import KrakNFT2 from '../../KrakNFT2.png';
-
-const cards = [0, 1, 2];
+import { data } from '../../CardData.js';
 
 function MarketplacePreview(props) {
-  const [page, setPage] = React.useState(1);
-  const handleChange = (event, value) => {
-    setPage(value);
-  };
 
-  const getImage = (index) => {
-    if (index === 0 ){
-      return KrakNFT0
-    }
-    else if (index === 1){
-      return KrakNFT1
-    }
-    else{
-      return KrakNFT2;
-    }
+  const [page, setCurrentPage] = React.useState(1);
+  const handleChange = (event, page) => {
+    setCurrentPage(page)
   }
 
   return (
     <>
-      <Box sx={{ bgcolor: 'background.paper', pt: 1, pb: 6, display: 'flex', flexGrow: 1, flexWrap: 'wrap' }}>
+      <Box sx={{ bgcolor: 'background.paper', pt: 1, pb: 6, display: 'flex', flexGrow: 1, flexWrap: 'wrap'}}>
         <Container sx={{ py: 1 }} maxWidth="md">
           <Box sx={{ display: 'flex' }}> 
-            {cards.map((card, index) => (
-              <NFTCard key={index} name={`NFT ${index}`} image={getImage(index)}/>
+            {data.map((cards, key) => (
+              <NFTCard key={data} name={data.name} image={data.image} />
             ))}
           </Box>
           <Stack sx={{ pt: 4 }} direction="row" spacing={2} justifyContent="center">
