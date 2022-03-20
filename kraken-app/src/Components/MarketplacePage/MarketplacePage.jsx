b37e0b7d68a6e8ffc969749e5695e12cd94ab2e1
Krystyna Urbanczyk <62307276+krystyna-urbanczyk@users.noreply.github.com>
2022-03-20 11:48:13 -0500

    added tabs to Marketplace page to follow our project proposal design (#12)

--- kraken-app/src/Components/MarketplacePage/MarketplacePage.jsx
+++ kraken-app/src/Components/MarketplacePage/MarketplacePage.jsx
@@ -2,23 +2,113 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 import NFTCard from '../NFTCard/NFTCard.jsx';
-import { data } from '../../CardData.js';
+//import { data } from '../../CardData.js';
+import KrakNFT0 from '../../KrakNFT0.png';
+import KrakNFT1 from '../../KrakNFT1.png';
+import KrakNFT2 from '../../KrakNFT2.png';
+
+const cards = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
 
 function MarketplacePage(props) {
 
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const getImage = (index) => {
+    if (index === 0 ){
+      return KrakNFT0
+    }
+    else if (index === 1){
+      return KrakNFT1
+    }
+    else{
+      return KrakNFT2;
+    }
+  }
+
   return (
     <>
+    <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+          <Tab label="Buy NFTs From Users" {...a11yProps(0)}/>
+          <Tab label="Buy NFTs From KrakNFT" {...a11yProps(1)}/>
+          <Tab label="Sell Your Owned NFTs" {...a11yProps(2)}/>
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0}>
         <Container sx={{ py: 8 }} maxWidth="xl">
           <Typography variant="h6" gutterBottom component="div">
-            KrakNFT Marketplace
+            NFTs for Sale by Users
           </Typography>
-          <Box sx={{ display: 'flex', flexWrap: 'wrap'}}> 
-            {data.map((cards, key) => (
-              <NFTCard key={data} name={data.name} image={data.image}/>
+          <Box sx={{ display: 'flex', flexWrap: 'wrap' }}> 
+            {cards.map((card, index) => (
+              <NFTCard name={`Nft ${index}`} image={getImage(index)}/>
             ))}
           </Box>
         </Container>
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <Container sx={{ py: 8 }} maxWidth="xl">
+          <Typography variant="h6" gutterBottom component="div">
+            NFTs for Sale by KrakNFT
+          </Typography>
+          <Box sx={{ display: 'flex', flexWrap: 'wrap' }}> 
+            {cards.map((card, index) => (
+              <NFTCard name={`Nft ${index}`} image={getImage(index)}/>
+            ))}
+          </Box>
+        </Container>
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        <Container sx={{ py: 8 }} maxWidth="xl">
+          <Typography variant="h6" gutterBottom component="div">
+            NFTs for Sale by You
+          </Typography>
+        </Container>
+      </TabPanel>
+    </Box>
     </>
   );
 
--- kraken-app/src/Components/MarketplacePreview/MarketplacePreview.jsx
+++ kraken-app/src/Components/MarketplacePreview/MarketplacePreview.jsx
@@ -4,9 +4,53 @@ import Container from '@mui/material/Container';
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
 import NFTCard from '../NFTCard/NFTCard.jsx';
-import { data } from '../../CardData.js';
+//import { data } from '../../CardData.js';
+import KrakNFT0 from '../../KrakNFT0.png';
+import KrakNFT1 from '../../KrakNFT1.png';
+import KrakNFT2 from '../../KrakNFT2.png';
+
+const cards = [0, 1, 2];
+
+const getImage = (index) => {
+  if (index === 0 ){
+    return KrakNFT0
+  }
+  else if (index === 1){
+    return KrakNFT1
+  }
+  else{
+    return KrakNFT2;
+  }
+}
 
 function MarketplacePreview(props) {
+  const [page, setPage] = React.useState(1);
+  const handleChange = (event, value) => {
+    setPage(value);
+  };
+
+  return (
+    <>
+    <Box sx={{ bgcolor: 'background.paper', pt: 8, pb: 6, display: 'flex', flexGrow: 1, flexWrap: 'wrap' }}>
+      <Container sx={{ py: 8 }} maxWidth="md">
+          <Box sx={{ display: 'flex' }}> 
+            {cards.map((card, index) => (
+              <Box sx={{ display: 'flex', p: 1, m: 1, flexGrow: 1 }}>
+                  <NFTCard name={`Nft ${index}`} image={getImage(index)}/>
+              </Box>
+            ))}
+          </Box>
+          <Stack sx={{ pt: 4 }} direction="row" spacing={2} justifyContent="center">
+          <Pagination count={3} page={page} onChange={handleChange} />
+          </Stack>
+        </Container>
+      </Box>
+    </>
+  );
+}
+export default MarketplacePreview;
+
+/*function MarketplacePreview(props) {
 
   const [page, setCurrentPage] = React.useState(1);
   const handleChange = (event, page) => {
@@ -31,4 +75,4 @@ function MarketplacePreview(props) {
   );
 
 }
-export default MarketplacePreview;
+export default MarketplacePreview;*/
