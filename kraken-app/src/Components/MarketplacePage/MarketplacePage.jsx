37c357b8bbb0a1ae5fb45cff2d2c6510a03ed812
Nicolas Infante <nicolas.alvaro.infante@gmail.com>
2022-04-02 11:21:14 -0500

    sizing for marketplace page fix

--- kraken-app/src/Components/MarketplacePage/MarketplacePage.jsx
+++ kraken-app/src/Components/MarketplacePage/MarketplacePage.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Grid } from '@mui/material';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
@@ -6,9 +7,14 @@ import PropTypes from 'prop-types';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import NFTCard from '../NFTCard/NFTCard.jsx';
-import { KrakenAPI } from '../../api.js';
+//import { data } from '../../CardData.js';
+import KrakNFT0 from '../../KrakNFT0.png';
+import KrakNFT1 from '../../KrakNFT1.png';
+import KrakNFT2 from '../../KrakNFT2.png';
 
-const api = new KrakenAPI()
+const name = [KrakNFT0, KrakNFT1, KrakNFT2, KrakNFT2, KrakNFT0, KrakNFT1, KrakNFT1, KrakNFT2, KrakNFT0];
+const title = ["Sea Kraken", "Dune Kraken", "Shadow Kraken", "Shadow Kraken", "Sea Kraken", "Dune Kraken", "Dune Kraken", "Shadow Kraken", "Sea Kraken"];
+
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -44,43 +50,89 @@ function a11yProps(index) {
 }
 
 function MarketplacePage(props) {
-  const [nfts, setNfts] = React.useState([])
-  const [value, setValue] = React.useState(0);
 
-  React.useEffect( () =>{
-    api.getMarketplaceNfts().then( (res) => setNfts(res))
-    
-  }, [])
+  const [value, setValue] = React.useState(0);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+
+  const getImage = (index) => {
+    if (index === 0 ){
+      return KrakNFT0
+    }
+    else if (index === 1){
+      return KrakNFT1
+    }
+    else{
+      return KrakNFT2;
+    }
+  }
+
+  const getName = (index) => {
+    for(var i=0; i<title.length; i++){
+      if (index === i){
+        return title[i]
+      }
+    }
+  }
 
   return (
     <>
     <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
         <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+          <Tab label="Buy NFTs From Users" {...a11yProps(0)}/>
           <Tab label="Buy NFTs From KrakNFT" {...a11yProps(1)}/>
-          <Tab label="BUY NFTS FROM USERS" {...a11yProps(2)}/>
+          <Tab label="Sell Your Owned NFTs" {...a11yProps(2)}/>
         </Tabs>
       </Box>
       <TabPanel value={value} index={0}>
-        <Container sx={{ py: 8 }} maxWidth="xl">
+        <Container sx={{ py: 2, width: '1000px'}} maxWidth="xl">
           <Typography variant="h6" gutterBottom component="div">
-            NFTs for Sale by KrakNFT
+            NFTs for Sale by Users
           </Typography>
-          <Box sx={{ display: 'flex', flexWrap: 'wrap' }}> 
-            {nfts.map((nft, index) => (
-                  <NFTCard key={index} nft={nft} name={nft.metadata.name} image={nft.metadata.image}/>
+          <Grid
+            sx={{ bgcolor: 'background.paper', pt: 8, pb: 8, display: 'flex', flexGrow: 1, flexWrap: 'wrap'}}
+            container
+            columnSpacing="row"
+            spacing={2}
+            className={MarketplacePage.container}
+            justifyContent="flex-end"
+          >
+            {name.map((card, index) => (
+              <Grid item xs={4} sm={4} md={3}>
+                    <NFTCard name={getName(index)} image={getImage(index)}/>
+              </Grid>
                   ))}
-          </Box>
+          </Grid>
         </Container>
       </TabPanel>
       <TabPanel value={value} index={1}>
-        <Container sx={{ py: 8 }} maxWidth="xl">
+        <Container sx={{ py: 2, width: '1000px' }} maxWidth="xl">
           <Typography variant="h6" gutterBottom component="div">
-            NFTs for Sale by Users
+            NFTs for Sale by KrakNFT
+          </Typography>
+          <Grid
+            sx={{ bgcolor: 'background.paper', pt: 8, pb: 8, display: 'flex', flexGrow: 1, flexWrap: 'wrap'}}
+            container
+            columnSpacing="row"
+            spacing={2}
+            className={MarketplacePage.container}
+            justifyContent="flex-end"
+          >
+            {nfts.map((nft, index) => (
+              <Grid item xs={4} sm={4} md={3}>
+-                  <NFTCard key={index} nft={nft} name={nft.metadata.name} image={nft.metadata.image}/>
+              </Grid>
+                  ))}
+          </Grid>
+        </Container>
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        <Container sx={{ py: 2 }} maxWidth="xl">
+          <Typography variant="h6" gutterBottom component="div">
+            NFTs for Sale by You
           </Typography>
         </Container>
       </TabPanel>
