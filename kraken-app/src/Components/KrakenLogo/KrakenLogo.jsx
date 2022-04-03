ffbad700fe63d3c5eb285147f9b7039ae4bbe70b
nathanclough <nathan.clough@ttu.edu>
2022-04-02 20:49:01 -0500

    fixed tests

--- kraken-app/src/Components/KrakenLogo/KrakenLogo.jsx
+++ kraken-app/src/Components/KrakenLogo/KrakenLogo.jsx
@@ -4,7 +4,6 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
-import KrakNFTLogo from "../../KrakNFTLogo.png";
 
 function KrakenLogo(props) {
   return (
@@ -13,7 +12,6 @@ function KrakenLogo(props) {
         <CardMedia
           component="img"
           style={{backgroundColor: "cyan"}}
-          image={KrakNFTLogo}
           alt="kraken logo"
         />
         <CardContent sx={{ display: 'flex', justifyContent: 'space-evenly', p: 1, m: 1, flexGrow: 1 }}>
