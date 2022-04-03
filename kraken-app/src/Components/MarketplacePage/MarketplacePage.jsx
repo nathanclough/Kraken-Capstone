582e13f1c1395bec96d1949517b83727f7d2a0f3
Nathan Clough <nathan.clough@ttu.edu>
2022-04-03 18:23:48 -0500

    Filter search (#25)
    * started the Filter_Search
    
    * Added sort and filter functions
    
    * Set scroll to top on view click
    
    * Fix build
    
    * Fixed build again
    
    Co-authored-by: Joseph-McClaine <joseph.mcclaine@ttu.edu>

--- kraken-app/src/Components/MarketplacePage/MarketplacePage.jsx
+++ kraken-app/src/Components/MarketplacePage/MarketplacePage.jsx
@@ -8,6 +8,7 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import NFTCard from '../NFTCard/NFTCard.jsx';
 import { KrakenAPI } from '../../api.js';
+import FilterSearch from '../FilterSearch/FilterSearch.jsx'
 
 const api = new KrakenAPI()
 
@@ -47,12 +48,35 @@ function a11yProps(index) {
 function MarketplacePage(props) {
   const [nfts, setNfts] = React.useState([])
   const [value, setValue] = React.useState(0);
+  const [search,setSearch] = React.useState({property: "name",value:""})
+
+  const sort = (prop,direction) =>{
+    var newNfts = [...nfts]
+    newNfts = sortFunc({"nfts" :newNfts,"prop":prop, "direction":direction})
+    setNfts(newNfts)
+    
+  }
+
+  const sortFunc =(args) =>{
+    args.nfts.sort((a, b) => (a.metadata[args.prop] > b.metadata[args.prop]) ? 1 : -1)
+    if(args.direction === "desc"){
+      args.nfts.reverse();
+    }
+
+    return args.nfts
+  }
 
   React.useEffect( () =>{
-    api.getMarketplaceNfts().then( (res) => setNfts(res))
-    
+    api.getMarketplaceNfts().then( (res) => {
+            var n = sortFunc({"nfts" :res,"prop":"name", "direction":"asc"})  
+            setNfts(n)
+           })
   }, [])
 
+  const setSearchVals = (args)=>{
+    setSearch({property:args.property,value: args.value})
+  }
+  
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
@@ -66,20 +90,24 @@ function MarketplacePage(props) {
           <Tab label="BUY NFTS FROM USERS" {...a11yProps(2)}/>
         </Tabs>
       </Box>
+      <FilterSearch setSearch={setSearchVals} search={search} sort={sort}></FilterSearch>
       <TabPanel value={value} index={0}>
         <Container sx={{ py: 8 }} maxWidth="xl">
           <Typography variant="h6" gutterBottom component="div">
             NFTs for Sale by KrakNFT
           </Typography>
           <Grid
-            sx={{ bgcolor: 'background.paper', pt: 8, pb: 8, display: 'flex', flexGrow: 1, flexWrap: 'wrap', flexDirection:'row-reverse'}}
+            sx={{ bgcolor: 'background.paper', pt: 8, pb: 8, display: 'flex', flexGrow: 1, flexWrap: 'wrap'}}
             container
             columnSpacing="row"
             spacing={2}
             className={MarketplacePage.container}
             justifyContent="flex-end"
           > 
-            {nfts.map((nft, index) => (
+            {nfts.filter(x => {              
+              var res = x.metadata[search.property].toLowerCase().includes(search.value.toLowerCase())
+              return res
+            }).map((nft, index) => (
               <Grid item xs={4} sm={4} md={3}>
                   <NFTCard key={index} nft={nft} name={nft.metadata.name} image={nft.metadata.image}/>
               </Grid>
